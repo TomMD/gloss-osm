7bb32cc54d5ea94806f1769d693ad2cd375d7197
Thomas M. DuBuisson <Thomas.DuBuisson@gmail.com>
2012-01-23 23:22:36 -0800

    Test program

---
+++ Test/Main.hs
@@ -0,0 +1,16 @@
+#!/usr/bin/env runhaskell
+import qualified Graphics.Gloss.OSM as OSM
+import Graphics.Gloss.Interface.IO.Simulate
+-- import Graphics.Gloss.Interface.Animate
+import Data.GPS
+
+main = do
+  svc <- OSM.startService
+  let wh   = (1680,1050)
+      zoom = 16
+      pnt  = (pt 45.5 (-122.5787) Nothing Nothing)
+      center = Color red (circleSolid 10)
+      serve _ = do
+         p <- OSM.serveBackground svc wh zoom pnt
+         return (Pictures [p,center])
+  simulateIO (FullScreen wh) white 1 () serve (\ _ _ _ -> return ())
